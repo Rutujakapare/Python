3005ff10fafae302de552f031391fbbc5efbac5b
Rutujakapare <kaparerutuja.entc@gmail.com>
2025-06-17 01:51:54 +0530

    list pattern, list joins and list unpacking

--- list_pattern.py
+++ list_pattern.py
@@ -2,3 +2,21 @@
 
 print(list(range(5,101,5)))
 
+
+##                     list joins
+### add in space or any symbol in between the list item
+
+sentence = ' '
+new_sentence =sentence.join(['welcome','to','python','coding','practice'])
+print(new_sentence)
+
+##                       OR
+print(' '.join (['welcome','to','python','coding','practice']))
+
+##                    list unpacking
+a,b,c,*other,d = [1,2,3,4,5,6,7,8,9]
+print(a)
+print(b)
+print(c)
+print ([*other])
+print(d)
